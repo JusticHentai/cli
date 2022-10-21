5127f044d34ceb238ebb20a7c90574ce962004f2
JusticHentai <1360636180@qq.com>
2022-10-21 17:52:56 +0800

    feat: 初始化项目

---
+++ index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+import clear from 'clear'
+import commandConfig from './src/commands'
+
+/**
+ * 总运行入口
+ * 清空当前命令行 并 初始化命令配置
+ */
+function init(): void {
+  clear()
+  commandConfig()
+}
+
+init()
